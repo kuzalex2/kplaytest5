51255758e1803de6e2ce3c170e416318b848eeab
kuzalex2 <me@kuzalex.com>
2019-11-18 19:25:45 +0300

    Initial Commit

---
+++ KPlayTest5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KPlayTest5
+//
+//  Created by kuzalex on 11/18/19.
+//  Copyright © 2019 kuzalex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
