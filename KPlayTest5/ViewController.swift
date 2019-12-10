71d5f360f0d651efd2290b4e9c89d3d47bfbf42c
kuzalex2 <me@kuzalex.com>
2019-12-10 18:35:08 +0300

    -

--- KPlayTest5/ViewController.swift
+++ KPlayTest5/ViewController.swift
@@ -521,8 +521,8 @@ class ViewController: UIViewController, KPlayerEvents {
 //            player = KTestRtmpAPlayGraph();
 //            player = KTestRtmpVPlayGraph(self.videoView);
 //            player = KTestRtmpAPlayAACGraph();
-//            player = KTestRtmpAVPlayGraph(self.videoView);
-            player = KTestRtmpAVQueuePlayGraph(self.videoView);
+            player = KTestRtmpAVPlayGraph(self.videoView);
+//            player = KTestRtmpAVQueuePlayGraph(self.videoView);
             
             player?.events = self
         }
@@ -530,11 +530,11 @@ class ViewController: UIViewController, KPlayerEvents {
 //        player?.play("rtmp://176.9.99.77:1935/vod/testa2.flv", autoStart: true);
 //        player?.play("rtmp://176.9.99.77:1935/vod/testa.mp4", autoStart: true);
 //        player?.play("rtmp://176.9.99.77:1935/vod/testa.flv", autoStart: true);
-        player?.play("rtmp://176.9.99.77:1935/vod/bb.mp4", autoStart: false);
+//        player?.play("rtmp://176.9.99.77:1935/vod/bb.mp4", autoStart: false);
 //        player?.play("rtmp://176.9.99.77:1935/vod/starwars_1080p.mp4", autoStart: true);
 //        player?.play("rtmp://176.9.99.77:1935/vod/bb.flv", autoStart: true);
 //        player?.play("rtmp://176.9.99.77:1935/vod/testamonoaac.flv", autoStart: true);
-//        player?.play("rtmp://176.9.99.77:1935/vod/test.mp4", autoStart: true);
+        player?.play("rtmp://176.9.99.77:1935/vod/test.mp4", autoStart: true);
 //        player?.play("rtmp://176.9.99.77:1935/myapp/stream", autoStart: true);
 //        player?.play("rtmp://176.9.99.77:1935/vod/testv.mp4", autoStart: false);
 //        player?.play("rtmp://176.9.99.77:1936/vod/test.mp4", autoStart: true);
